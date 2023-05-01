a12d7618dfd471f81c0e369233c0f60bc1318ddf
Alexander <soldatovalex207z@gmail.com>
2023-05-01 16:45:01 +0300

    before changes of parsing

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api/([0-9]{4}-[0-1]{1}[0-9]{1}-[0-1]{1}[0-9]{1})', (req, res) => { 
+app.get('/api', (req, res) => {
+  let utcDate = new Date();
+
+  res.send({unix: utcDate.getTime(), utc:utcDate.toUTCString()});
+});
+
+app.get('/api/([0-9]{4}-{0,}[0-9]{0,2}-{0,}[0-9]{0,2})', (req, res) => {
   let route = req.path;
   let routeDate = '';
   for(let i = 5; i < route.length; i++)
@@ -40,10 +47,13 @@ app.get('/api/([0-9]{4}-[0-1]{1}[0-9]{1}-[0-1]{1}[0-9]{1})', (req, res) => {
     return arrDataNumber.push(Number(el));
   });
   console.log(arrDataNumber);
-  arrDataNumber[1]--;
+  if(arrDataNumber.length > 1)
+    arrDataNumber[1]--;
+    if(arrDataNumber[1] > 12) res.send({ error : "Invalid Date" });
+
   let utcData = new Date(Date.UTC(...arrDataNumber));
   let unixData = utcData.getTime()
-  res.json({unix: unixData, utc: utcData.toUTCString()});
+  res.send({unix: unixData, utc: utcData.toUTCString()});
 });
 
 
@@ -56,12 +66,13 @@ app.get('/api/([0-9]{1,})', (req, res) => {
   }
   let unixData = Number(routeDate);
   let utcData = new Date(unixData);
-  res.json({unix: routeDate, utc: utcData});
+  res.send({unix: unixData, utc: utcData.toUTCString()});
 });
 
 app.get('/api/(.{1,})', (req, res) => {
-  res.json({error: 'Invalid Date'});
+  res.send({ error : "Invalid Date" });
 })
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
